12282691ed3773d1bded9d7c01d04a95da61964e
= <=>
2022-12-14 09:40:27 +0100

    first commit

---
+++ page-butikker.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>butikker</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://kristinesdomain.com/mmd_eksamensprojekt_peech/wordpress/kontakt/wp-content/themes/divi_child/style.css"/>
+    
+    <style> 
+body {
+  background-image: url('.../langbaggrund.jpg');
+}
+
+
+#overskrift h1 {
+    color: #10163E;
+  }
+
+#img p {
+    color: #10163E;
+}
+</style>
+  </head>
+    <?php get_header(); ?>
+ 
+    <body>
+
+
+<main class="container-fluid">
+
+
+
+<section id="overskrift" class="row m-2 pt-5">
+  <div class="top">
+<h1 class="mt-5 mb-5">Du er altid velkommen til at besøge Peech</h1>
+</div>
+</section>
+
+<section id="img" class="row">
+  
+<div class="col-lg-6">
+<!-- <div class="col-lg-6"> -->
+  <div class="img_one"><img class="img-fluid" src="http://kristinesdomain.com/mmd_eksamensprojekt_peech/wordpress/wp-content/themes/divi_child/img/img_one.jpg" alt="kontakt_img_one"></div>
+  <div class="text_one m-2 text-center"><p class="p_one">Mandag-Lørdag 12-19</p><p class="p_two">Søndag Lukket</p></div>
+<!-- </div> -->
+</div>
+
+
+<div class="col-lg-6">
+<!-- <div class="col-lg-6"> -->
+<div class="img_two"><img class="img-fluid" src="http://kristinesdomain.com/mmd_eksamensprojekt_peech/wordpress/wp-content/themes/divi_child/img/img_two.jpg" alt="kontakt_img_two"></div>
+<div class="text_two m-2 text-center"><p class="p_two_one">Søndag-Mandag Lukket</p><p class="p_two_two">Tirsdag-Fredag 12-19</p><p class="p_two_three">Lørdag 11-19</p></div>
+<!-- </div> -->
+</div>
+</section>
+
+<section id="third" class="row">
+<h3 class="mt-5 mb-5 text-center">Vi glæder os til se jer!</h3>
+</section>
+
+
+<!-- <section id="stiftere_indhold" class="et_pb_section et_pb_section_4 et_pb_with_background et_section_regular" style="background-color:#000000">
+<h1>Hejsa</h1>
+</section> -->
+
+</main>
+
+
+
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/ScrollTrigger.min.js"></script>
+<script src="http://asoep.dk/kea/post/wp-content/themes/Divi-child/js/script.js"></script> -->
+
+
+<?php get_footer(); ?>
+</body>
+
+  </html>
